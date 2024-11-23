1be103dc99578c8813596c492a977d92b5658202
pakshal26-droid <pakshaljain54@gmail.com>
2024-11-24 00:56:59 +0530

    user controller and route

--- src/app.js
+++ src/app.js
@@ -15,4 +15,9 @@ app.use(express.json({ limit: "16kb" })); //middleware for accepting json
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //to get params from url
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//import routes
+import userRouter from "./routes/user.routes.js";
+//routes declaration
+app.use("/api/v1/users", userRouter);
 export { app };
